72c6f86936713c3ea993536c55720164bf052747
seongho9 <seongho9276@gmail.com>
2024-06-19 17:42:22 +0900

    [FEAT] Media tag domain

--- media/Media_Engine/Media_Engine/include/ICE/SDP.hpp
+++ media/Media_Engine/Media_Engine/include/ICE/SDP.hpp
@@ -4,7 +4,9 @@
 #define SDP_PACKET_HPP
 
 #include <string>
+#include <sstream>
 #include <vector>
+#include <spdlog/spdlog.h>
 
 namespace media_engine 
 {
@@ -21,7 +23,24 @@ namespace media_engine
 			std::string to_string();
 
 			void set_version(short version);
-			void set_
+			void set_origin(Origin origin);
+			void set_session_name(std::string session_name);
+			void set_information(std::string info);
+			void set_uri(std::string uri);
+			void set_phone(std::string phone);
+			int set_media(Media media);
+			int set_attribute(Attribute attribute);
+
+		private:
+			short _version;
+			Origin _origin;
+			std::string _session_name;
+			std::string _information;
+			std::string _uri;
+			std::string _phone;
+			std::vector<Media> _media;
+			std::vector<Attribute> _attri;
+
 		};
 
 		enum Address_Type
@@ -55,7 +74,7 @@ namespace media_engine
 			std::string _session_id;
 			Network_Type _network_type;
 			Address_Type _address_type;
-			std::stringn _address;
+			std::string _address;
 		};
 
 		enum Protocol
@@ -63,35 +82,6 @@ namespace media_engine
 			UDP,
 			RTP_AVP,
 			RTP_SAVP
-		};
-
-		class Media
-		{
-		public:
-			Media();
-			Media(std::string param);
-			Media(Media& rhs);
-
-			std::string to_string();
-
-			void set_media(std::string media);
-			void set_port(int port);
-			void set_protocol(Protocol proto);
-			int set_format(int format);
-			int set_attribute(Attribute attri);
-
-		private:
-			//	type of media( audio, video, etc )
-			std::string _media;
-			//	port number send or receive ( get at least one port)
-			std::vector<int> _port;
-			//	transport protocol
-			Protocol _protocol;
-			//	media format( protocol number is predefined )
-			std::vector<int> _format;
-			//	mapping attribute
-			std::vector<Attribute> _attribute;
-
 		};
 
 		class Attribute
@@ -109,6 +99,52 @@ namespace media_engine
 		private:
 			std::string _key;
 			std::string _value;
+		};
+
+		class Media
+		{
+		public:
+			Media();
+			Media(std::string param);
+			Media(Media& rhs);
+			Media(Media&& rhs);
+
+			std::string to_string();
+
+			void set_media(const std::string& media);
+
+			void set_port(const int& port);
+			void set_port(const std::string& port);
+			void set_port(const std::vector<int>& ports);
+
+			void set_protocol(const Protocol& proto);
+			void set_protocol(const std::string& proto);
+
+			void set_format(const int& format);
+			void set_format(const std::string& format);
+			void set_format(const std::vector<int>& formats);
+
+			void set_attribute(const Attribute& attri);
+			void set_attribute(const std::vector<Attribute>& attri);
+
+			std::string get_media() const;
+			std::vector<int> get_port() const;
+			Protocol get_protocol()	const;
+			std::vector<int> get_format() const;
+			std::vector<Attribute> get_attribute() const;
+
+		private:
+			//	type of media( audio, video, etc )
+			std::string *_media;
+			//	port number send or receive ( get at least one port)
+			std::vector<int> *_port;
+			//	transport protocol
+			Protocol *_protocol;
+			//	media format( protocol number is predefined )
+			std::vector<int> *_format;
+			//	mapping attribute
+			std::vector<Attribute> *_attribute;
+
 		};
 	}
 }
